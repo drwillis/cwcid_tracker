54b089d761db84e87f2145c4a1d7dc2ff59f6bc0
drwillis <arwillis@uncc.edu>
2025-02-01 08:32:42 -0500

    Create cwcid_default_auth_credentials.py
    Authorization credentials for SMTP mail access and overleaf access.

---
+++ cwcid_default_auth_credentials.py
@@ -0,0 +1,16 @@
+# Example:
+#   smtp_server = "smtp.gmail.com"
+#   smtp_port = 587
+#   sender_email = "me@gmail.com"  # Replace with your email
+#   sender_password = "aaaa bbbb cccc dddd"  # Replace with your email password or app-specific password
+email_auth_dict = {
+    "smtp_server": "smtp.gmail.com",
+    "smtp_port": 587,
+    "sender_email": "me@gmail.com",  # Replace with your email
+    "sender_password": "aaaa bbbb cccc dddd"  # Replace with your email password or app-specific password
+}
+# Replace with your Git repository username and access token
+overleaf_auth_dict = {
+    "username": "git",
+    "token": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
